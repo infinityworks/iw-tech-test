2de35559e235b6fb1125e66c0fb0567650791553
Jake Hall <jake.hall@infinityworks.com>
2018-04-12 15:15:56 +0100

    Implement the authorities endpoint (#2)

--- src/main/java/com/infinityworks/techtest/app/Application.java
+++ src/main/java/com/infinityworks/techtest/app/Application.java
@@ -2,7 +2,9 @@ package com.infinityworks.techtest.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @ComponentScan("com.infinityworks.techtest")
@@ -10,6 +12,11 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public RestTemplate getRestTemplateBean(){
+        return new RestTemplate();
     }
 
 }
---
+++ src/main/java/com/infinityworks/techtest/model/FSAAuthorityList.java
@@ -0,0 +1,45 @@
+package com.infinityworks.techtest.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+
+public class FSAAuthorityList {
+
+    @JsonProperty("authorities")
+    private List<FSAAuthority> fsaAuthorityList;
+
+    public FSAAuthorityList() {
+        //blank required for jackson
+    }
+
+    public FSAAuthorityList(List<FSAAuthority> fsaAuthorityList) {
+        this.fsaAuthorityList = fsaAuthorityList;
+    }
+
+    public List<FSAAuthority> getFsaAuthorityList() {
+        return fsaAuthorityList;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        FSAAuthorityList that = (FSAAuthorityList) o;
+
+        return fsaAuthorityList != null ? fsaAuthorityList.equals(that.fsaAuthorityList) : that.fsaAuthorityList == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return fsaAuthorityList != null ? fsaAuthorityList.hashCode() : 0;
+    }
+
+    @Override
+    public String toString() {
+        return "FSAAuthorityList{" +
+                "fsaAuthorityList=" + fsaAuthorityList +
+                '}';
+    }
+}
